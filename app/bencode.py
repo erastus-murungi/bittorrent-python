6254feee451b97939fb5f254339abfb5aa6c4934
Erastus Murungi <erastuswagwan@gmail.com>
2023-10-23 11:28:58 -0700

    Calculate info hash

--- app/bencode.py
+++ app/bencode.py
@@ -1,15 +1,17 @@
+import hashlib
+from pathlib import Path
 from pprint import pprint
 
 from app.utils import check_state
 
+BEncodedInteger = int
+BEncodedString = str
+BEncodedList = list[BEncodedInteger | BEncodedString]
+BEncodedDictionary = dict[str, BEncodedInteger | BEncodedString | BEncodedList]
+BEncodedValue = BEncodedInteger | BEncodedString | BEncodedList | BEncodedDictionary
 
-def parse_out_integer(encoded_str: str) -> tuple[int, str]:
-    check_state(encoded_str[0] == "i", "Encoded integer must start with 'i'")
-    int_end_index = encoded_str.index("e", 0)
-    return int(encoded_str[1:int_end_index]), encoded_str[int_end_index + 1 :]
 
-
-def decode(encoded_value_bytes_or_str: bytes | str) -> str | int | list[int | str]:
+def bencode_decode(encoded_value_bytes_or_str: bytes | str) -> BEncodedValue:
     encoded_value = (
         encoded_value_bytes_or_str.decode()
         if isinstance(encoded_value_bytes_or_str, bytes)
@@ -27,7 +29,13 @@ def decode(encoded_value_bytes_or_str: bytes | str) -> str | int | list[int | st
         raise NotImplementedError(f"Unknown encoded value {encoded_value}")
 
 
-def decode_string(encoded_string: str) -> str:
+def parse_integer(encoded_str: str) -> tuple[BEncodedInteger, str]:
+    check_state(encoded_str[0] == "i", "Encoded integer must start with 'i'")
+    int_end_index = encoded_str.index("e", 0)
+    return int(encoded_str[1:int_end_index]), encoded_str[int_end_index + 1 :]
+
+
+def decode_string(encoded_string: str) -> BEncodedString:
     """
     >>> decode_string("5:hello")
     'hello'
@@ -38,14 +46,14 @@ def decode_string(encoded_string: str) -> str:
         encoded_string = encoded_string.decode()
     length, string = encoded_string.split(":", maxsplit=1)
     decoded = string[: int(length)]
-    # check_state(
-    #     len(decoded) == int(length),
-    #     "Length of decoded string does not match length in encoded string",
-    # )
+    check_state(
+        len(decoded) == int(length),
+        "Length of decoded string does not match length in encoded string",
+    )
     return decoded
 
 
-def decode_integer(encoded_integer: str) -> int:
+def decode_integer(encoded_integer: str) -> BEncodedInteger:
     """
     >>> decode_integer("i123e")
     123
@@ -61,13 +69,30 @@ def decode_integer(encoded_integer: str) -> int:
     return int(encoded_integer[1:-1])
 
 
-def parse_element(encoded_value: str) -> tuple[int | str | list[int | str], str]:
+def parse_string(encoded_value: str) -> tuple[str, BEncodedString]:
+    """
+    >>> parse_string("5:hello")
+    ('hello', '')
+    >>> parse_string("10:hello12345")
+    ('hello12345', '')
+    """
+    check_state(encoded_value[0].isdigit(), "Encoded string must start with a digit")
+    length, string = encoded_value.split(":", maxsplit=1)
+    decoded = string[: int(length)]
+    check_state(
+        len(decoded) == int(length),
+        "Length of decoded string does not match length in encoded string",
+    )
+    return decoded, string[int(length) :]
+
+
+def parse_element(
+    encoded_value: str,
+) -> tuple[BEncodedValue, str]:
     if encoded_value[0] == "i":
-        return parse_out_integer(encoded_value)
+        return parse_integer(encoded_value)
     elif encoded_value[0].isdigit():
-        num_chars = encoded_value.split(":", 1)[0]
-        len_count = len(num_chars) + 1 + int(num_chars)
-        return decode_string(encoded_value[:len_count]), encoded_value[len_count:]
+        return parse_string(encoded_value)
     elif encoded_value[0] == "l":
         return decode_list(encoded_value), ""
     elif encoded_value[0] == "d":
@@ -76,7 +101,7 @@ def parse_element(encoded_value: str) -> tuple[int | str | list[int | str], str]
         raise NotImplementedError(f"Unknown encoded value {encoded_value}")
 
 
-def decode_list(encoded_list: str) -> list[int | str]:
+def decode_list(encoded_list: str) -> BEncodedList:
     check_state(
         encoded_list[0] == "l" and encoded_list[-1] == "e",
         'Encoded list must start with "l" and end with "e"',
@@ -89,7 +114,7 @@ def decode_list(encoded_list: str) -> list[int | str]:
     return decoded_list
 
 
-def decode_dictionaries(encoded_dict: str) -> dict[str, int | str | list[int | str]]:
+def decode_dictionaries(encoded_dict: str) -> BEncodedDictionary:
     check_state(
         encoded_dict[0] == "d" and encoded_dict[-1] == "e",
         'Encoded dictionary must start with "d" and end with "e"',
@@ -103,17 +128,61 @@ def decode_dictionaries(encoded_dict: str) -> dict[str, int | str | list[int | s
     return decoded_dict
 
 
+def bencode_encode(value: BEncodedValue) -> str:
+    if isinstance(value, int):
+        return encode_integer(value)
+    elif isinstance(value, str):
+        return encode_string(value)
+    elif isinstance(value, list):
+        return encode_list(value)
+    elif isinstance(value, dict):
+        return encode_dict(value)
+    else:
+        raise NotImplementedError(f"Unknown value {value}")
+
+
+def encode_integer(value: BEncodedInteger) -> str:
+    return f"i{value}e"
+
+
+def encode_string(value: BEncodedString) -> str:
+    return f"{len(value)}:{value}"
+
+
+def encode_list(value: BEncodedList) -> str:
+    return "l" + "".join(map(bencode_encode, value)) + "e"
+
+
+def encode_dict(value: BEncodedDictionary) -> str:
+    return f'd{"".join(bencode_encode(key) + bencode_encode(value) for key, value in value.items())}e'
+
+
 def parse_torrent(
     torrent_filename: str | bytes,
-) -> dict[str, int | str | list[int | str]]:
+) -> BEncodedDictionary:
     torrent_filename = (
         torrent_filename
         if isinstance(torrent_filename, str)
         else torrent_filename.decode()
     )
-    with open(torrent_filename, "rb") as f:
-        encoded_dict = f.read().decode(errors="replace")
-        return decode_dictionaries(encoded_dict)
+    encoded_dict = Path(torrent_filename).read_bytes().decode("latin-1")
+    check_state(
+        encoded_dict[0] == "d" and encoded_dict[-1] == "e",
+        'Encoded dictionary must start with "d" and end with "e"',
+    )
+    decoded_dict = {}
+    encoded_value: str = encoded_dict[1:-1]
+    while encoded_value:
+        key, encoded_value = parse_string(encoded_value)
+        value, encoded_value = parse_element(encoded_value)
+        decoded_dict[key] = value
+    return decoded_dict
+
+
+def calc_info_hash(meta_info: BEncodedDictionary):
+    info = meta_info["info"]
+    info_bytes = bencode_encode(info).encode("latin-1")
+    return hashlib.sha1(info_bytes).hexdigest()
 
 
 if __name__ == "__main__":
@@ -121,4 +190,6 @@ if __name__ == "__main__":
     #
     # doctest.testmod()
     # print(decode(b"d3:foo3:bar5:helloi52ee"))
-    pprint(parse_torrent(b"../sample.torrent"))
+    metainfo = parse_torrent(b"../sample.torrent")
+    pprint(metainfo)
+    print(calc_info_hash(metainfo))
--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import json
 import sys
 
-from app.bencode import decode as decode_bencode, parse_torrent
+from app.bencode import bencode_decode, parse_torrent, calc_info_hash
 
 
 def main():
@@ -16,13 +16,14 @@ def main():
     if command == "decode":
         bencoded_value = sys.argv[2].encode()
         # Uncomment this block to pass the first stage
-        print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
+        print(json.dumps(bencode_decode(bencoded_value), default=bytes_to_str))
     elif command == "info":
         torrent_file = sys.argv[2].encode()
         metainfo = parse_torrent(torrent_file)
         print(
             f'Tracker URL: {metainfo["announce"]}\n'
-            f'Length: {metainfo["info"]["length"]}'
+            f'Length: {metainfo["info"]["length"]}\n'
+            f"Info Hash: {calc_info_hash(metainfo)}"
         )
     else:
         raise NotImplementedError(f"Unknown command {command}")
