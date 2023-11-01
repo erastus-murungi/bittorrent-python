b59aa2158da760dfa34ac54162d0efdfd727caba
Erastus Murungi <erastuswagwan@gmail.com>
2023-11-01 15:24:47 -0700

    added docs for peer messages

--- app/messages.py
+++ app/messages.py
@@ -1,28 +1,68 @@
 from __future__ import annotations
 
 import struct
-from abc import abstractmethod
+from abc import abstractmethod, ABC
 from dataclasses import dataclass
 from typing import Final
 
 
 @dataclass
-class PeerMessage:
-    # The length prefix is a four byte big-endian value
+class PeerMessage(ABC):
+    """
+    Base class for all peer messages.
+
+    Peer Messages are a part of the BitTorrent protocol and are used to facilitate
+    communication between peers in a swarm.
+
+    Attributes:
+        len (int): The length prefix is a four byte big-endian value.
+                   It represents the length of the message, not including the length prefix itself.
+        id (int): ID of the message.
+    """
+
     len: int
     id: int
 
 
-class FixedLengthPeerMessage(PeerMessage):
+class FixedLengthPeerMessage(PeerMessage, ABC):
+    """
+    Base class for fixed length peer messages.
+
+    FixedLengthPeerMessage is a specialized type of PeerMessage where the message length is fixed.
+    """
+
+    @staticmethod
+    @abstractmethod
+    def pack() -> bytes:
+        """
+        Packs a message into bytes for transmission.
+
+        Returns:
+            bytes: Byte representation of the message.
+        """
+        pass
+
     @staticmethod
     @abstractmethod
     def get():
+        """
+        Returns an instance of a message.
+
+        Returns:
+            PeerMessage: An instance of a specific PeerMessage subclass.
+        """
         pass
 
 
 @dataclass
 class KeepAlive(FixedLengthPeerMessage):
-    # keep-alive: <len=0000>
+    """
+    Keep-alive messages with length prefix set to zero are periodically sent to maintain the connection alive.
+
+    BitTorrent Specification:
+    keep-alive: <len=0000>
+    """
+
     len: int = 0
     id: int = -1
 
@@ -37,7 +77,13 @@ class KeepAlive(FixedLengthPeerMessage):
 
 @dataclass
 class Choke(FixedLengthPeerMessage):
-    # choke: <len=0001><id=0>
+    """
+    The choke message is used to tell the peer that it should stop sending requests.
+
+    BitTorrent Specification:
+    choke: <len=0001><id=0>
+    """
+
     len: int = 1
     id: int = 0
 
@@ -52,7 +98,13 @@ class Choke(FixedLengthPeerMessage):
 
 @dataclass
 class Unchoke(FixedLengthPeerMessage):
-    # unchoke: <len=0001><id=1>
+    """
+    The unchoke message is used to tell the peer that it may continue sending requests.
+
+    BitTorrent Specification:
+    unchoke: <len=0001><id=1>
+    """
+
     len: int = 1
     id: int = 1
 
@@ -67,7 +119,13 @@ class Unchoke(FixedLengthPeerMessage):
 
 @dataclass
 class Interested(FixedLengthPeerMessage):
-    # interested: <len=0001><id=2>
+    """
+    The interested message is used to tell the peer that the sender is interested in downloading.
+
+    BitTorrent Specification:
+    interested: <len=0001><id=2>
+    """
+
     len: int = 1
     id: int = 2
 
@@ -82,7 +140,13 @@ class Interested(FixedLengthPeerMessage):
 
 @dataclass
 class NotInterested(FixedLengthPeerMessage):
-    # not interested: <len=0001><id=3>
+    """
+    The not interested message is used to tell the peer that the sender is not interested in downloading.
+
+    BitTorrent Specification:
+    not interested: <len=0001><id=3>
+    """
+
     len: int = 1
     id: int = 3
 
@@ -97,7 +161,13 @@ class NotInterested(FixedLengthPeerMessage):
 
 @dataclass(kw_only=True)
 class Have(PeerMessage):
-    # have: <len=0005><id=4><piece index>
+    """
+    The "have" message is used to tell the peer that the sender has downloaded a piece.
+
+    BitTorrent Specification:
+    have: <len=0005><id=4><piece index>
+    """
+
     piece_index: int
     len: int = 5
     id: int = 4
@@ -113,7 +183,13 @@ class Have(PeerMessage):
 
 @dataclass(kw_only=True)
 class BitField(PeerMessage):
-    # bitfield: <len=0001+X><id=5><bitfield>
+    """
+    The bitfield message is used to exchange information about pieces that a peer has.
+
+    BitTorrent Specification:
+    bitfield: <len=0001+X><id=5><bitfield> where X is the length of the bitfield.
+    """
+
     bitfield: bytes
     id: int = 5
 
@@ -130,21 +206,21 @@ class BitField(PeerMessage):
 
 @dataclass(kw_only=True)
 class Request(PeerMessage):
-    # request: <len=0013><id=6><index><begin><length>
+    """
+    The request message is used to request a block of a piece from a peer.
 
-    # integer specifying the zero-based piece index
+    BitTorrent Specification:
+    request: <len=0013><id=6><index><begin><length>
+    """
+
     index: int
-
-    # integer specifying the zero-based byte offset within the piece
     begin: int
-
-    # integer specifying the requested length
     length: int
-
     id: int = 6
     len: int = 13
 
     def pack(self):
+        """See base class."""
         return struct.pack(
             "!IBIII", Request.len, Request.id, self.index, self.begin, self.length
         )
@@ -159,11 +235,16 @@ class Request(PeerMessage):
 
 @dataclass(kw_only=True)
 class Piece(PeerMessage):
-    # piece: <len=0009+X><id=7><index><begin><block>
+    """
+    The piece message is used to deliver a block of a piece that has been requested.
+
+    BitTorrent Specification:
+    piece: <len=0009+X><id=7><index><begin><block> where X is the length of the block.
+    """
+
     index: int
     begin: int
     block: bytes
-
     id: int = 7
 
     def __post_init__(self):
@@ -181,11 +262,16 @@ class Piece(PeerMessage):
 
 @dataclass(kw_only=True)
 class Cancel(PeerMessage):
-    # cancel: <len=0013><id=8><index><begin><length>
+    """
+    The cancel message is used to cancel a block request.
+
+    BitTorrent Specification:
+    cancel: <len=0013><id=8><index><begin><length>
+    """
+
     index: int
     begin: int
     length: int
-
     id: int = 8
     len: int = 13
 
@@ -204,13 +290,19 @@ class Cancel(PeerMessage):
 
 @dataclass(kw_only=True)
 class Port(PeerMessage):
-    # port: <len=0003><id=9><listen-port>
-    listen_port: int
+    """
+    The port message is used to tell the peer which port the sender is listening on.
 
+    BitTorrent Specification:
+    port: <len=0003><id=9><listen-port>
+    """
+
+    listen_port: int
     id: int = 9
     len: int = 3
 
     def pack(self):
+        """See base class."""
         return struct.pack("!IBH", Port.len, Port.id, self.listen_port)
 
     @staticmethod
@@ -221,7 +313,12 @@ class Port(PeerMessage):
 
 @dataclass(slots=True, kw_only=True)
 class HandShake:
-    # pstr: string identifier of the protocol
+    """
+    The handshake message is the first message sent and then received from a remote peer.
+
+    BitTorrent Specification:
+    handshake: <pstrlen><pstr><reserved><info_hash><peer_id>
+    """
     peer_id: bytes
     info_hash: bytes
     reserved: bytes = 8 * b"\x00"
@@ -230,28 +327,17 @@ class HandShake:
 
     def __post_init__(self):
         assert len(self.pstr) == self.pstrlen, "pstrlen does not match pstr length"
-
         assert len(self.reserved) == 8, "reserved does not match reserved length"
         assert len(self.info_hash) == 20, "info_hash is not 20 bytes"
         assert len(self.peer_id) == 20, "peer_id is not 20 bytes"
 
     def pack(self):
-        # handshake: <pstrlen><pstr><reserved><info_hash><peer_id>
-        # return (
-        #     self.pstrlen.to_bytes(1, "big")
-        #     + self.pstr
-        #     + self.reserved
-        #     + self.info_hash
-        #     + self.peer_id
-        # )
-
         return struct.pack(
             ">B19s8x20s20s", self.pstrlen, self.pstr, self.info_hash, self.peer_id
         )
 
     @staticmethod
     def decode(resp: bytes):
-        # handshake: <pstrlen><pstr><reserved><info_hash><peer_id>
         (
             _,
             _,
@@ -262,6 +348,6 @@ class HandShake:
 
 
 if __name__ == "__main__":
-    print(KeepAlive.id)
-    print(Interested())
-    print(Port(listen_port=3))
+    import doctest
+
+    doctest.testmod()
